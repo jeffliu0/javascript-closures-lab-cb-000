4112e628a62c44b92f5b68cdf8de958f609c57d5
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-24 22:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function bumpCounter(){
 function createAnimal(animalType){
   function animalCreated(deadlyDevice){
     return {
-      this.animalType: animalType
-      this.deadlyDevice: deadlyDevice
+      animalType: animalType
+      deadlyDevice: deadlyDevice
     }
   }
 }
